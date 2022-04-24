cac5c8c27a44123528330819302c04d4a7ec6635
Garry Trinder <garry@trinder365.co.uk>
2022-04-24 20:45:19 +0100

    Fixes tests

--- src/commands.spec.ts
+++ src/commands.spec.ts
@@ -6,23 +6,23 @@ describe('commands', () => {
 
     describe('getLoginCommand', () => {
 
-        // it('returns correct command with options', () => {
-        //     const options: Options = {
-        //         ADMIN_USERNAME: 'adminUsername',
-        //         ADMIN_PASSWORD: 'adminPassword'
-        //     };
-        //     const expected = 'login --authType password --userName adminUsername --password adminPassword';
-        //     const actual = getLoginCommand(options);
-        //     assert.equal(actual, expected);
-        // });
+        it('returns correct command with options', () => {
+            const options: Options = {
+                ADMIN_USERNAME: 'adminUsername',
+                ADMIN_PASSWORD: 'adminPassword'
+            };
+            const expected = 'login --authType password --userName adminUsername --password adminPassword';
+            const actual = getLoginCommand(options);
+            assert.equal(actual, expected);
+        });
 
-        // it('throws error if ADMIN_USERNAME not passed', () => {
-        //     const options: Options = {
-        //         ADMIN_USERNAME: '',
-        //         ADMIN_PASSWORD: 'adminPassword'
-        //     };
-        //     assert.throws(() => getLoginCommand(options), Error, 'ADMIN_USERNAME is required');
-        // });
+        it('throws error if ADMIN_USERNAME not passed', () => {
+            const options: Options = {
+                ADMIN_USERNAME: '',
+                ADMIN_PASSWORD: 'adminPassword'
+            };
+            assert.throws(() => getLoginCommand(options), Error, 'ADMIN_USERNAME is required');
+        });
 
         it('throws error if ADMIN_PASSWORD not passed', () => {
             const options: Options = {
